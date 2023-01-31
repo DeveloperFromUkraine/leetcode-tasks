fc19086966bb59c0e7b6997dc4a715651d6d1546
Volodymyr Kozlov <vkozlov@lohika.com>
2023-01-31 10:54:09 +0200

    add new completed tasks

--- easy/fibonacci.js
+++ easy/fibonacci.js
@@ -10,15 +10,8 @@ var fib = [0, 1]; // Initialize array!
 
 for (i = 2; i <= 10; i++) {
     // Next fibonacci number = previous + one before previous
-    // Translated to JavaScript:
     fib[i] = fib[i - 2] + fib[i - 1];
     console.log(fib[i]);
 }
 
 console.log(fib)
-
-
-// The first five principles are principles of class design. They are:
-//     L	The Liskov Substitution Principle	Derived classes must be substitutable for their base classes.
-//     I	The Interface Segregation Principle	Make fine grained interfaces that are client specific.
-//     D	The Dependency Inversion Principle	Depend on abstractions, not on concretions.
